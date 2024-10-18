c15da0a75efcbe402cf01c3f49f86b1d7546d640
hanyikao <48291489+hanyikao@users.noreply.github.com>
2024-10-18 23:03:53 +0800

    Update saving_utils.py
    Modify the id of gdrive url

--- utils/saving_utils.py
+++ utils/saving_utils.py
@@ -49,7 +49,7 @@ def check_or_download_model(file_path):
     import gdown
     if not os.path.exists(file_path):
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        url = "https://drive.google.com/uc?id=11xTBALOeUkyuaK3l60CpkYHLTmv7k3dY"
+        url = "https://drive.google.com/uc?id=1FwqWgrHvW0MYDhhakSiwhIMBf_RvySaz"
         gdown.download(url, file_path, quiet=False)
         print("Model downloaded successfully.")
     else:
